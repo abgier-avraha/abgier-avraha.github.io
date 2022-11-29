2100384b2363ea2af1c408c84124aa0dae14f393
Abgier Avraha <abgier.avraha@gmail.com>
2022-11-30 02:31:16 +1100

    lookin good

---
+++ src/config.ts
@@ -0,0 +1,42 @@
+import type { SocialObjects } from "./types";
+
+export const SITE = {
+  website: "https://abgier-avraha.github.io/",
+  author: "Abgier Avraha",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  title: "Producing Value Blog",
+  // TODO: replace image
+  ogImage: "default-og.png",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+};
+
+// TODO: replace and enable image
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: false,
+  width: 216,
+  height: 46,
+};
+// TODO: replace favicon
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/abgier-avraha",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://au.linkedin.com/in/abgier-avraha-240510126",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:abgier.avraha@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+];
---
+++ src/pages/about.md
@@ -0,0 +1,6 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+---
+
+A blog made using the Astro Paper theme https://github.com/satnaing/astro-paper.
